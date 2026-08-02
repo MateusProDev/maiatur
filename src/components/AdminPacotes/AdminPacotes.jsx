9b11bb84fe97ead659a3aab0360769198a9ef767
mateusprodev <mateusferreiraprodev@gmail.com>
2026-08-02 12:58:00 -0300

    add novos campos no addmin do pacotes

--- src/components/AdminPacotes/AdminPacotes.jsx
+++ src/components/AdminPacotes/AdminPacotes.jsx
@@ -836,7 +836,7 @@ const AdminPacotes = () => {
               />
             </Grid>
             
-            <Grid item xs={12}>
+            <Grid item xs={12}> 
               <TextField
                 fullWidth
                 label="Descrição Curta"
